cfd3fcf90d220af57159e5180449021bc11577fa
Denis Githuku <denisgithuku07@gmail.com>
2026-01-22 22:24:05 +0300

    Implement supplier data source

--- data/src/main/kotlin/com/githukudenis/smartduka/data/datasource/local/SupplierLocalDataSource.kt
+++ data/src/main/kotlin/com/githukudenis/smartduka/data/datasource/local/SupplierLocalDataSource.kt
@@ -15,4 +15,41 @@
 */
 package com.githukudenis.smartduka.data.datasource.local
 
+import com.githukudenis.smartduka.database.dao.SupplierDao
+import com.githukudenis.smartduka.database.entity.SupplierEntity
+import kotlinx.coroutines.flow.Flow
+
 // Local datasource for supplier data
+interface SupplierLocalDataSource {
+    suspend fun insertSupplier(supplier: SupplierEntity)
+
+    suspend fun updateSupplier(supplier: SupplierEntity)
+
+    suspend fun archiveSupplier(supplierId: String)
+
+    suspend fun getSupplierById(supplierId: String): SupplierEntity?
+
+    suspend fun observeSuppliersByShop(shopId: String): Flow<List<SupplierEntity>>
+}
+
+class SupplierLocalDataSourceImpl(private val supplierDao: SupplierDao) : SupplierLocalDataSource {
+    override suspend fun insertSupplier(supplier: SupplierEntity) {
+        supplierDao.insert(supplier)
+    }
+
+    override suspend fun updateSupplier(supplier: SupplierEntity) {
+        supplierDao.update(supplier)
+    }
+
+    override suspend fun archiveSupplier(supplierId: String) {
+        supplierDao.archive(supplierId)
+    }
+
+    override suspend fun getSupplierById(supplierId: String): SupplierEntity? {
+        return supplierDao.getById(supplierId)
+    }
+
+    override suspend fun observeSuppliersByShop(shopId: String): Flow<List<SupplierEntity>> {
+        return supplierDao.observeByShop(shopId)
+    }
+}
