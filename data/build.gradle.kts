b76a37a58fd120d97050ed91759809e6f4c76a60
Denis Githuku <denisgithuku07@gmail.com>
2026-01-23 12:54:32 +0300

    Add unit test library

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.smartduka.android.library)
     alias(libs.plugins.smartduka.convention.koin)
+    alias(libs.plugins.smartduka.unit.test)
 }
 
 android { namespace = "com.githukudenis.smartduka.data" }
