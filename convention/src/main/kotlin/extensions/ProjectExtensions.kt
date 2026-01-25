2c3492842c6ba1ce2ac60d79c18b5adcf8bd31cc
Denis Githuku <denisgithuku07@gmail.com>
2026-01-25 12:18:26 +0300

    Update compose testing dependencies

--- convention/src/main/kotlin/extensions/ProjectExtensions.kt
+++ convention/src/main/kotlin/extensions/ProjectExtensions.kt
@@ -1,6 +1,7 @@
 package extensions
 
 import com.android.build.api.dsl.CommonExtension
+import libs
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.dependencies
@@ -35,15 +36,14 @@ internal fun Project.configureAndroidCompose(
             val bom = libs.findLibrary("compose-bom").get()
             implementation(platform(bom))
             implementation(libs.getBundle("compose"))
+
+            // Testing
             androidTestImplementation(platform(bom))
             debugImplementation(libs.getLibrary("compose-ui-tooling"))
             debugImplementation(libs.getLibrary("compose-ui-test-manifest"))
             androidTestImplementation(libs.getLibrary("compose-ui-test-junit4"))
-            androidTestImplementation("androidx.test:runner:1.4.0")
-            androidTestImplementation("androidx.test.ext:junit:1.1.3")
             // compose navigation
-            implementation(libs.getLibrary("androidx-navigation-compose"))
-            testImplementation(libs.getLibrary("androidx-navigation-testing"))
+            androidTestImplementation(libs.getLibrary("androidx-navigation-testing"))
         }
     }
 }
